93fbb85a92e8008c13f23043993ec6b8b54b5404
Marbeck <raphael@watchnext.fr>
2024-12-01 14:48:57 +0100

    Day1 OK (renaming)

--- day1/src/main/scala/Solution.scala
+++ day1/src/main/scala/Solution.scala
@@ -1,22 +1,22 @@
 object Solution:
   def run(inputLines: Seq[String]): (String, String) =
 
-    val (list1, list2) = inputLines.collect:
+    val (left, right) = inputLines.collect:
       case s"$first   $second" => (first.toInt, second.toInt)
     .unzip
 
-    val res1 = list1.sorted.zip(list2.sorted).map:
-      case (first, second) => Math.abs(first - second)
+    val part1Result = left.sorted.zip(right.sorted).map:
+      case (fromLeft, fromRight) => Math.abs(fromLeft - fromRight)
     .sum
 
-    val List(grouped1, grouped2) = List(list1, list2).map(_.groupMapReduce(identity)(_ => 1)(_ + _))
+    val List(leftOccurrences, rightOccurrences) = List(left, right).map(_.groupMapReduce(identity)(_ => 1)(_ + _))
 
-    val res2 = grouped1.map:
-      case (value, nbIn1) => value * (nbIn1 * grouped2.getOrElse(value, 0))
+    val part2Result = leftOccurrences.map:
+      case (value, nbIn1) => value * (nbIn1 * rightOccurrences.getOrElse(value, 0))
     .sum
 
-    val result1 = s"$res1"
-    val result2 = s"$res2"
+    val result1 = s"$part1Result"
+    val result2 = s"$part2Result"
 
     (s"$result1", s"$result2")
 
