01b366ff182f43928c2afdba6fc9085cad2e6b6a
rmarbeck <rafoufou>
2024-12-03 10:38:33 +0100

    Day 3 OK

---
+++ day3/src/main/scala/Solution.scala
@@ -0,0 +1,20 @@
+import scala.util.matching.Regex
+
+object Solution:
+  def run(inputLines: Seq[String]): (String, String) =
+
+    val inputAsOneLine = inputLines.mkString
+    val inputCleaned = inputAsOneLine.replaceAll("""don\'t\(\)((?!do\(\)).)*""", "")
+
+    val List(part1, part2) = List(inputAsOneLine, inputCleaned).map(multiply).map(_.sum)
+
+    val result1 = s"$part1"
+    val result2 = s"$part2"
+
+    (s"$result1", s"$result2")
+
+end Solution
+
+val Mul: Regex = """mul\((\d+),(\d+)\)""".r
+
+def multiply(input: String): Iterator[Long] = for case Mul(first, second) <- Mul.findAllIn(input) yield first.toLong * second.toLong
