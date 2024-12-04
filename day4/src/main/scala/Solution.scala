92f3d36056f9f2250f2ed161c1f4d253ea4a6a74
Marbeck <raphael@watchnext.fr>
2024-12-04 11:44:03 +0100

    Day 4 OK (adding comments)

--- day4/src/main/scala/Solution.scala
+++ day4/src/main/scala/Solution.scala
@@ -15,36 +15,31 @@ object Solution:
         if grid(x)(y) == 'X'
       yield
         (for
-          dirX <- List(-1, 0, 1)
-          dirY <- List(-1, 0, 1)
+          dirX <- List(-1, 0, 1) // exploring in each direction on x
+          dirY <- List(-1, 0, 1) // exploring in each direction on y
         yield
-          val List(posX, posY) = List(dirX, dirY).map(dir => List(1 ,2 ,3).map(_ * dir))
-          val word = posX.zip(posY).map((curX, curY) => readChar(x + curX, y + curY)).mkString("")
-          word match
-            case "MAS" => 1
-            case _ => 0
-          ).sum
-        ).sum
+          val List(driftX, driftY) = List(dirX, dirY).map(dir => List(1 ,2 ,3).map(_ * dir)) // looking for 3 letters
+          val word = driftX.zip(driftY).map((curX, curY) => readChar(x + curX, y + curY)).mkString("")
+          word == "MAS"
+        ).count(identity)
+      ).sum
 
 
     val result2 =
       (for
-        x <- grid.indices
-        y <- grid(x).indices
+        x <- grid.indices.tail // no need to explore first row
+        y <- grid(x).indices.tail // no need to explore first col
         if grid(x)(y) == 'A'
       yield
         val words =
-          val posX = List(1, -1, -1, 1)
-          val posY = List(-1, 1, -1, 1)
-          posX.zip(posY).map:
+          val driftX = List(1, -1, -1, 1)  // looking for 4 letters around
+          val driftY = List(-1, 1, -1, 1)  // diagonally
+          driftX.zip(driftY).map:
             case (curX, curY) => readChar(x + curX, y + curY)
           .mkString("").grouped(2)
 
-        if words.forall(word => word.count(_ == 'S') == 1 && word.count(_ == 'M') == 1) then
-          1
-        else
-          0
-        ).sum
+        words.forall(word => word.contains('S') && word.contains('M'))
+      ).count(identity)
 
     (s"$result1", s"$result2")
 
