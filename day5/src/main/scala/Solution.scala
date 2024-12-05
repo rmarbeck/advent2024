6b7ce8de6832ff511be2845dac6c49ef003dd783
rmarbeck <rafoufou>
2024-12-05 11:20:01 +0100

    Day 5 OK

--- day5/src/main/scala/Solution.scala
+++ day5/src/main/scala/Solution.scala
@@ -1,42 +1,76 @@
 import scala.annotation.tailrec
 
+type RuleSet = Map[Int, List[Int]]
+type Update = List[Int]
+
 object Solution:
   def run(inputLines: Seq[String]): (String, String) =
 
-    val (rules, updates) = inputLines.foldLeft((Map(), Nil): (Map[Int, List[Int]], List[List[Int]])):
-      case (acc, s"$before|$after") =>
-        acc._1.get(before.toInt) match
-          case Some(existingList) => (acc._1 + (before.toInt -> (after.toInt :: existingList)), acc._2)
-          case None => (acc._1 + (before.toInt -> (after.toInt :: Nil)), acc._2)
-      case (acc, values) if values != "" => (acc._1, values.split(",").map(_.toInt).toList :: acc._2)
+    val (rules, updates) = inputLines.foldLeft((Map(), Nil): (RuleSet, List[Update])):
+      case (acc, RuleExt(rule)) => (rule.addToRulSet(acc._1), acc._2)
+      case (acc, UpdateExt(values*)) => (acc._1, values.toList :: acc._2)
       case (acc, _) => acc
 
-    println(updates.filter(isValid(_, rules)).map(middle).sum)
+    given RuleSet = rules
 
-    //
-    // Code is here
-    //
+    val (validReports, invalidReports) = updates.partition(isValid)
 
+    val invalidReportsSorted = invalidReports.map:
+      update =>
+        given Update = update
+        update.sorted(using SpecialOrder().updateOrd)
 
-
-    val result1 = s""
-    val result2 = s""
+    val List(result1, result2) = List(validReports, invalidReportsSorted).map(_.map(middle).sum)
 
     (s"$result1", s"$result2")
 
+
 def middle(inList: List[Int]): Int = inList(inList.length / 2)
 
-def isValid(update: List[Int], rules: Map[Int, List[Int]]): Boolean =
+def isValid(update: List[Int])(using rules: RuleSet): Boolean =
   @tailrec
   def check(before: List[Int], next: List[Int]): Boolean =
     next match
+      case Nil => true
       case head :: tail =>
         rules.get(head) match
           case None => check(head :: before, tail)
           case Some(constraints) =>
             constraints intersect before match
-              case head :: tail => false
               case Nil => check(head :: before, tail)
-      case Nil => true
+              case _ => false
 
   check(List(update.head), update.tail)
+
+//Ordering for part 2
+class SpecialOrder(using rules: RuleSet, update: Update):
+  given updateOrd: Ordering[Int] with
+    override def compare(x: Int, y: Int): Int =
+      def currentSituationInList: Int = (update.indexOf(x) - update.indexOf(y)).sign
+      (rules.get(y), rules.get(x)) match
+        case (Some(value), _) if value.contains(x) => -1
+        case (_, Some(value)) if value.contains(y) => 1
+        case _ => currentSituationInList
+
+case class Rule(before: Int, after: Int):
+  def addToRulSet(rules: RuleSet): RuleSet =
+    rules.get(before) match
+      case Some(previousFollowers) => rules.updated(before, after :: previousFollowers)
+      case None => rules + (before -> List(after))
+
+//Extractors
+object RuleExt:
+  def apply(rule: Rule): String = s"${rule.before}|${rule.after}"
+  def unapply(input: String): Option[Rule] =
+    input match
+      case s"$before|$after" => Some(Rule(before.toInt, after.toInt))
+      case _ => None
+
+object UpdateExt:
+  def apply(parts: Int*): String =
+    parts.mkString(",")
+  def unapplySeq(whole: String): Option[Seq[Int]] =
+    whole match
+      case "" => None
+      case _ => Some(whole.split(",").map(_.toInt).toSeq)
+
--- day5/src/test/scala/MySuite.scala
+++ day5/src/test/scala/MySuite.scala
@@ -3,10 +3,10 @@
 class MySuite extends munit.FunSuite:
   test("Day5 : main test on final data"):
     val (score1, score2) = Solver.solve
-    assertEquals(score1, "")
-    assertEquals(score2, "")
+    assertEquals(score1, "6242")
+    assertEquals(score2, "5169")
 
   test("Day5 : main test on initial data"):
     val (score1, score2) = Solver.solveTest
-    assertEquals(score1, "")
-    assertEquals(score2, "")
+    assertEquals(score1, "143")
+    assertEquals(score2, "123")
