c6bc873cefbf784557e9d44a5242766fd1b44bc7
Marbeck <raphael@watchnext.fr>
2024-12-08 17:19:31 +0100

    Day 8 OK (small refactoring)

--- day8/src/main/scala/Solution.scala
+++ day8/src/main/scala/Solution.scala
@@ -7,22 +7,21 @@ object Solution:
 
     given grid: Grid = IArray.from(inputLines.map(IArray.from))
 
-    val antennasPositionsGrouped = (for
-      x <- grid.indices
-      y <- grid(0).indices
-      currentAntenna = grid(x)(y)
-      if currentAntenna != '.'
-    yield
-      Antenna(Position(x, y), currentAntenna)
-      ).groupMap(_.value)(_.position).values.filter(_.size >= 2)
+    val antennasPositionsGrouped =
+      (for
+        x <- grid.indices; y <- grid(0).indices
+        currentAntenna = grid(x)(y); if currentAntenna != '.'
+      yield
+        Antenna(Position(x, y), currentAntenna)
+        ).groupMap(_.value)(_.position).values.filter(_.size >= 2)
 
-    val (antinodes, antinodes2) =
+    val (antinodesPart1, antinodesPart2) =
       antennasPositionsGrouped.foldLeft((Set(), Set())):
         case (acc, positions) => (acc._1 ++ guessAntinodes(Part1)(positions), acc._2 ++ guessAntinodes(Part2)(positions))
 
-    val forResult2 = antennasPositionsGrouped.flatten.toSet ++ antinodes2
+    val forResult2 = antennasPositionsGrouped.flatten.toSet ++ antinodesPart2
 
-    val result1 = antinodes.size
+    val result1 = antinodesPart1.size
     val result2 = forResult2.size
 
     (s"$result1", s"$result2")
@@ -31,7 +30,7 @@ case class Position(x: Int, y: Int):
   def isDefined(using grid: Grid): Boolean = grid.isDefinedAt(x) && grid(x).isDefinedAt(y)
 
   private def antiNode(to: Position, distance: Int = 2): Position =
-    def calcCoord(thisCoord: Int, otherCoord: Int): Int = (distance-1) * (thisCoord - otherCoord) + thisCoord
+    def calcCoord: (Int, Int) => Int = distance * _ - (distance - 1) * _
     Position(calcCoord(x, to.x), calcCoord(y, to.y))
 
   def findAntinodes(other: Position)(using grid: Grid): Set[Position] =  Set(this.antiNode(other), other.antiNode(this)).filter(_.isDefined)
@@ -39,13 +38,9 @@ case class Position(x: Int, y: Int):
   def findAntinodesWithResonance(distance: Int)(other: Position)(using grid: Grid): Set[Position] =
     @tailrec
     def findAntinodesWithResonanceRec(distance: Int)(other: Position, found: Set[Position]): Set[Position] =
-      val first = this.antiNode(other, distance)
-      val second = other.antiNode(this, distance)
-
-      if (first.isDefined || second.isDefined)
-        findAntinodesWithResonanceRec(distance + 1)(other, found ++ Set(first, second).filter(_.isDefined))
-      else
-        found
+      List(this.antiNode(other, distance), other.antiNode(this, distance)).filter(_.isDefined) match
+        case Nil => found
+        case newAntinodes => findAntinodesWithResonanceRec(distance + 1)(other, found ++ newAntinodes.toSet)
 
     findAntinodesWithResonanceRec(distance)(other, Set())
 
