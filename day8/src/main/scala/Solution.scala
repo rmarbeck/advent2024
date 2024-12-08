1734b43f206de67a256b48ce632c689372e89cf6
Marbeck <raphael@watchnext.fr>
2024-12-08 11:35:37 +0100

    Day 8 OK

---
+++ day8/src/main/scala/Solution.scala
@@ -0,0 +1,69 @@
+import scala.annotation.tailrec
+
+type Grid = IArray[IArray[Char]]
+
+object Solution:
+  def run(inputLines: Seq[String]): (String, String) =
+
+    given grid: Grid = IArray.from(inputLines.map(IArray.from))
+
+    val antennas = (for
+      x <- grid.indices
+      y <- grid(0).indices
+      currentAntenna = grid(x)(y)
+      if currentAntenna != '.'
+    yield
+      Antenna(Position(x, y), currentAntenna)
+      ).groupMap(_.value)(_.position)
+
+    val antinodes = antennas.values.flatMap(guessAntinodes(Part1)).toSet
+    val antinodes2 = antennas.values.flatMap(guessAntinodes(Part2)).toSet
+
+    val forResult2 = antennas.filter(_._2.size >= 2).flatMap(_._2).toSet ++ antinodes2
+
+    val result1 = antinodes.size
+    val result2 = forResult2.size
+
+    (s"$result1", s"$result2")
+
+case class Position(x: Int, y: Int):
+  def isDefined(using grid: Grid): Boolean = grid.isDefinedAt(x) && grid(x).isDefinedAt(y)
+
+  private def antiNode(to: Position, distance: Int = 2): Position =
+    def calcCoord(thisCoord: Int, otherCoord: Int): Int = (distance-1) * (thisCoord - otherCoord) + thisCoord
+    Position(calcCoord(x, to.x), calcCoord(y, to.y))
+
+  def findAntinodes(other: Position)(using grid: Grid): Set[Position] =  Set(this.antiNode(other), other.antiNode(this)).filter(_.isDefined)
+
+  def findAntinodesWithResonance(distance: Int)(other: Position)(using grid: Grid): Set[Position] =
+    @tailrec
+    def findAntinodesWithResonanceRec(distance: Int)(other: Position, found: Set[Position]): Set[Position] =
+      val first = this.antiNode(other, distance)
+      val second = other.antiNode(this, distance)
+
+      if (first.isDefined || second.isDefined)
+        findAntinodesWithResonanceRec(distance + 1)(other, found ++ Set(first, second).filter(_.isDefined))
+      else
+        found
+
+    findAntinodesWithResonanceRec(distance)(other, Set())
+
+case class Antenna(position: Position, value: Char)
+
+def guessAntinodes(part: Part)(positions: Seq[Position])(using grid: Grid): Set[Position] =
+  val finder: Position => Position => Set[Position] =
+    part match
+      case Part1 => _.findAntinodes
+      case Part2 => _.findAntinodesWithResonance(2)
+
+  @tailrec
+  def doCount(toExplore: List[Position], found: Set[Position]): Set[Position] =
+    toExplore match
+      case Nil | _ :: Nil => found
+      case head :: tail => doCount(tail, found ++ tail.flatMap(finder.apply(_).apply(head)))
+
+  doCount(positions.toList, Set())
+
+enum Part:
+  case Part1; case Part2
+export Part._
