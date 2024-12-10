d0d32297a3e72a3b8e42771c27074a9c85d53557
Marbeck <raphael@watchnext.fr>
2024-12-10 07:57:28 +0100

    Day 9 work in progress

--- day9/src/main/scala/Solution.scala
+++ day9/src/main/scala/Solution.scala
@@ -8,6 +8,7 @@ object Solution:
       case (size, index) => Block(index, size.head.asDigit, size.last.asDigit)
 
     println(compute(blocks))
+    println("\n"+computePart2(blocks))
 
     val result1 = s""
     val result2 = s""
@@ -19,7 +20,11 @@ case class Block(id: Int,fileLength: Int, free: Int):
   def splitAt(length: Int): (Block, Block) = (this.copy(fileLength = length, free = 0), this.copy(fileLength = fileLength - length, free = free))
   def sumAt(startIndex: Int): Long = id * ((2 * startIndex + fileLength - 1) * fileLength / 2f).toLong
 
+  def expandFrom(other: Block): Block = this.copy(free = this.free + other.fileLength + other.free)
+
   def freeIt: Block = this.copy(fileLength = 0)
+
+  override def toString: String = id.toString*fileLength + "."*free
 
 def compute(blocks: Array[Block]): Long =
   @tailrec
@@ -48,3 +53,28 @@ def compute(blocks: Array[Block]): Long =
         computeRec(blocksDQ, index + headBlock.fileLength, value + headBlock.sumAt(index))
 
   computeRec(mutable.ArrayDeque.from(blocks), 0, 0L)
+
+def computePart2(blocks: Array[Block]): Long =
+  @tailrec
+  def computeRec(blocksDQ: mutable.ArrayDeque[Block], index: Int): Array[Block] =
+    blocksDQ.foreach(print)
+    println
+    index match
+      case 0 => blocksDQ.toArray
+      case value =>
+        val freePlaceToFind = blocksDQ(value).fileLength
+        println(freePlaceToFind)
+        val found = blocksDQ.zipWithIndex.find(_._1.free >= freePlaceToFind)
+        found match
+          case None => computeRec(blocksDQ, index - 1)
+          case Some(block, innerIndex) =>
+            blocksDQ.update(innerIndex, block.copy(free = 0))
+            blocksDQ.insert(innerIndex + 1, blocksDQ(value).copy(free = block.free - freePlaceToFind))
+            blocksDQ.remove(index + 1)
+            blocksDQ.update(index, blocksDQ(index).copy(free = blocksDQ(index).free + block.fileLength + block.free))
+            computeRec(blocksDQ, blocksDQ.length - 1)
+
+  computeRec(mutable.ArrayDeque.from(blocks), blocks.length - 1).zipWithIndex.foldLeft(0L):
+    case (acc, (block, index)) =>
+      print(block)
+      acc + block.sumAt(index)
