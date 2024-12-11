3f05e03148b3b532035bafca879f8133ca063d0a
rmarbeck <rafoufou>
2024-12-11 09:45:29 +0100

    Day 11 OK

--- day11/src/main/scala/Solution.scala
+++ day11/src/main/scala/Solution.scala
@@ -1,30 +1,58 @@
 import scala.annotation.tailrec
+import scala.collection.mutable
+import scala.collection.mutable.Map
+
+type Stone = Long
+type Frequency = Long
 
 object Solution:
   def run(inputLines: Seq[String]): (String, String) =
 
-    val stones = inputLines.head.split(" ").map(_.toLong).map(Stone.apply).toList
+    val stones = inputLines.head.split(" ").map(_.toLong).toList
 
-    println(calculate(stones, 25).length)
-
-
-
-    val result1 = s""
-    val result2 = s""
+    val result1 = s"${calc2(stones, 25)}"
+    val result2 = s"${calc2(stones, 75)}"
 
     (s"$result1", s"$result2")
 
-case class Stone(value: Long):
+extension (value: Long)
   def next: Seq[Stone] =
     value match
-      case 0 => List(Stone(1))
+      case 0 => List(1)
       case value if value.toString.length % 2 ==0 =>
         val (left, right) = value.toString.splitAt(value.toString.length / 2)
-        List(Stone(left.toLong), Stone(right.toLong))
-      case other => List(Stone(value * 2024))
+        List(left.toLong, right.toLong)
+      case other => List(value * 2024)
+
+extension (stones: List[Stone])
+  def toFrequencyList: List[(Stone, Frequency)] = stones.groupMapReduce(identity)(_ => 1L)(_ + _).toList
 
 @tailrec
-def calculate(stones: List[Stone], remaingingSteps: Int): List[Stone] =
+def calculate(stones: List[Stone], remainingSteps: Int): List[Stone] =
+  remainingSteps match
+    case 0 => stones
+    case other => calculate(stones.flatMap(_.next), remainingSteps - 1)
+
+object Cache:
+  private val cache: mutable.Map[Stone, List[(Stone, Frequency)]] = mutable.Map()
+
+  def calculateBy5Cached(stone: Stone): List[(Stone, Frequency)] =
+    cache.getOrElseUpdate(stone, calculateBy5(stone))
+
+def calculateBy5(stone: Stone): List[(Stone, Frequency)] =
+  calculate(List(stone), 5).toFrequencyList
+
+def calc2(stones: List[Stone], remainingSteps: Int): Long =
+  calculate2(stones.toFrequencyList, remainingSteps).map(_._2).sum
+
+@tailrec
+def calculate2(stones: List[(Stone, Frequency)], remaingingSteps: Int): List[(Stone, Frequency)] =
   remaingingSteps match
     case 0 => stones
-    case other => calculate(stones.flatMap(_.next), remaingingSteps - 1)
+    case other =>
+      val test = stones.flatMap:
+        case (value, frequency) => (calculateBy5(value).map:
+          case (stone, subFrequency) => (stone, subFrequency * frequency)
+          )
+
+      calculate2(test.groupMapReduce(_._1)(_._2)(_ + _).toList ,remaingingSteps - 5)
--- day11/src/test/scala/MySuite.scala
+++ day11/src/test/scala/MySuite.scala
@@ -3,10 +3,10 @@
 class MySuite extends munit.FunSuite:
   test("Day11 : main test on final data"):
     val (score1, score2) = Solver.solve
-    assertEquals(score1, "")
-    assertEquals(score2, "")
+    assertEquals(score1, "185894")
+    assertEquals(score2, "221632504974231")
 
   test("Day11 : main test on initial data"):
     val (score1, score2) = Solver.solveTest
-    assertEquals(score1, "")
-    assertEquals(score2, "")
+    assertEquals(score1, "55312")
+    assertEquals(score2, "65601038650482")
