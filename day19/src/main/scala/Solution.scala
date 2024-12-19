d648b299bd6d3a4b6ef99644db7319afb2a1d2e5
Marbeck <raphael@watchnext.fr>
2024-12-19 16:43:47 +0100

    Day 19 OK

--- day19/src/main/scala/Solution.scala
+++ day19/src/main/scala/Solution.scala
@@ -1,65 +1,51 @@
 import scala.annotation.tailrec
+import scala.collection.mutable
 
 type Towels = Seq[Towel]
 
 object Solution:
   def run(inputLines: Seq[String]): (String, String) =
 
-    given  Towels = inputLines.head.split(",").map(_.trim).map(Towel.apply).toSeq
-
+    given Towels = inputLines.head.split(",").map(_.trim).map(Towel.apply).toSeq
     val designs = inputLines.drop(2)
+    given cache: Cache = Cache()
 
-    val result1 = designs.filter(d =>isValid(List(d)))
-
-    val result2 = result1.map(d =>ways2(List((d, Nil)))).map(_.length).sum
-
-    result1.map(d => ways2(List((d, Nil)))).foreach:
-      case ways =>
-        println(s"${ways.head.reverse.mkString} -> ${ways.length}")
-        println(s"\t${ways.map(_.reverse.mkString(",")).mkString(" - ")}");
-
+    val result1 = designs.filter(cache.isValidCached)
+    val result2 = result1.map(cache.waysCached).sum
 
     (s"${result1.length}", s"$result2")
 
 case class Towel(stripes: String):
+  lazy val length = stripes.length
   override def toString: String = stripes
 
-@tailrec
-def isValid(remaining: Seq[String], explored: Seq[String] = Nil)(using towels: Towels): Boolean =
+  def dropBy(design: String): Option[String] =
+    design.startsWith(stripes) match
+      case true => Some(design.drop(length))
+      case false => None
+
+
+class Cache:
+  given Cache = this
+  private val isValidCache: mutable.Map[String, Boolean] = mutable.Map()
+  def isValidCached(input: String)(using towels: Towels): Boolean =
+    isValidCache.getOrElseUpdate(input, isValid(List(input)))
+
+  private val nbOfWaysCache: mutable.Map[String, Long] = mutable.Map()
+  def waysCached(input: String)(using towels: Towels): Long =
+    nbOfWaysCache.getOrElseUpdate(input, successfullWays(input))
+
+def isValid(remaining: Seq[String])(using towels: Towels, cache: Cache): Boolean =
   remaining match
     case Nil => false
     case head :: tail =>
-      towels.exists(_.stripes == head) match
-        case true => true
-        case false =>
-          val remainingToAppend = towels.map(_.stripes).filter(head.startsWith).map(t => head.drop(t.length)).filterNot(explored.contains)
-          isValid(remainingToAppend ++: remaining.tail, head +: explored)
+      if towels.exists(_.stripes == head) then
+        true
+      else
+        towels.flatMap(_.dropBy(head)).exists(cache.isValidCached)
 
-@tailrec
-def ways2(remaining: Seq[(String, List[Towel])], explored: Seq[(String, List[Towel])] = Nil, found: Seq[List[Towel]] = Nil)(using towels: Towels): Seq[List[Towel]] =
-  remaining match
-    case Nil => found
-    case (current @ ("", towelsUpToNow)) :: tail =>
-      ways2(remaining.tail, current +: explored, towelsUpToNow +: found)
-    case (current @ (head, towelsUpToNow)) :: tail =>
-      val remainingToAppend =
-        towels.filter(t => head.startsWith(t.stripes)).map(t => (head.drop(t.stripes.length), t :: towelsUpToNow)).filterNot(explored.contains)
-
-      ways2(remainingToAppend ++: remaining.tail, current +: explored, found)
-
-@tailrec
-def ways(remaining: Seq[(String, List[Towel])], explored: Seq[String] = Nil, found: Seq[List[Towel]] = Nil)(using towels: Towels): Seq[List[Towel]] =
-  remaining match
-    case Nil => found
-    case (head, towelsUpToNow) :: tail =>
-      towels.find(_.stripes == head) match
-        case Some(towel) =>
-          val remainingToAppend =
-            towels.filterNot(_ == towel).filter(t => head.startsWith(t.stripes)).map(t => (head.drop(t.stripes.length), t :: towelsUpToNow)).filterNot((currentString, _) => explored.contains(currentString))
-
-          ways(remainingToAppend ++: remaining.tail, explored, (towel :: towelsUpToNow) +: found)
-        case None =>
-          val remainingToAppend =
-            towels.filter(t => head.startsWith(t.stripes)).map(t => (head.drop(t.stripes.length), t :: towelsUpToNow)).filterNot((currentString, _) =>explored.contains(currentString))
-
-          ways(remainingToAppend ++: remaining.tail, head +: explored, found)
+def successfullWays(subDesign: String)(using towels: Towels, cache: Cache): Long =
+  subDesign match
+    case "" => 1
+    case _ =>
+      towels.flatMap(_.dropBy(subDesign)).filter(current => current.isBlank || cache.isValidCached(current)).map(cache.waysCached).sum
