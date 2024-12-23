5c45f3176166da38d4fd1166fcffbaa91961ebb6
rmarbeck <rmarbeck@free.fr>
2024-12-23 22:58:54 +0100

    Day 23 OK

--- day23/src/main/scala/Solution.scala
+++ day23/src/main/scala/Solution.scala
@@ -12,18 +12,13 @@ object Solution:
 
     given Links = grouped
 
-    val result = grouped.filter(_._1.startsWith("t")).flatMap((key, values) => connected(values.toList, Nil).map((f, s) => alpha(key, f, s))).toList.distinct
+    val result1 = grouped.filter(_._1.startsWith("t")).flatMap((key, values) => connected(values.toList, Nil).map((f, s) => alpha(key, f, s))).toList.distinct
 
     import Order.given
 
+    val result2 = findBest(TreeSet(grouped.map((key, values) => values + key).map(_.toList).toList:_ *))
 
-    println(grouped.map((key, values) => values + key).map(_.toList))
-    val resultb = findBest(TreeSet(grouped.map((key, values) => values + key).map(_.toList).toList:_ *))
-
-    val result1 = s"${result}"
-    val result2 = s"$resultb"
-
-    (s"$result1", s"$result2")
+    (s"${result1.size}", s"$result2")
 
 object Order:
   given orderingList: Ordering[List[String]] = Ordering.by(str => (-str.length, str.sorted.mkString))
@@ -32,9 +27,9 @@ def alpha(one: String, two: String, three: String): String =
   List(one, two, three).sorted.mkString("-")
 
 @tailrec
-def findBest(toExplore: TreeSet[List[String]])(using links: Links): Int =
+def findBest(toExplore: TreeSet[List[String]])(using links: Links): String =
   toExplore match
-    case empty if empty.isEmpty => 0
+    case empty if empty.isEmpty => ""
     case notEmpty =>
       val (head, tail) = (notEmpty.head, notEmpty.tail)
       val matches = head.forall:
@@ -42,7 +37,7 @@ def findBest(toExplore: TreeSet[List[String]])(using links: Links): Int =
           val next = links(member) + member
           head.forall(next.contains)
       matches match
-        case true => head.size
+        case true => head.sorted.mkString(",")
         case false => findBest(tail ++ head.combinations(head.size - 1))
 
 @tailrec
@@ -53,21 +48,3 @@ def connected(list: List[String], current: List[(String, String)])(using links: 
       val updatedList = tail.collect:
         case currentComputerInTail if links(currentComputerInTail).contains(head) => (currentComputerInTail, head)
       connected(tail, updatedList ::: current)
-
-def best(setOfComputers: Set[String])(using links: Links): List[String] =
-  setOfComputers match
-    case empty if empty.isEmpty => Nil
-    case notEmpty =>
-      val (head, tail, currentSize) = (notEmpty.head, notEmpty.tail, notEmpty.size)
-      tail.foldLeft(links(head) + head):
-        case (acc, currentComputer) => acc intersect (links(currentComputer) + currentComputer)
-      match
-        case value if value.size == currentSize => value.toList
-        case other =>
-          setOfComputers.toList.combinations(currentSize - 1).map(subList => best(subList.toSet)).maxBy(_.length)
-
-
-
-def findSetConnected(computer: String)(using links: Links): Int =
-  val attached = links(computer)
-  attached.map(cu => (links(cu) intersect(attached + computer)).size).max
--- day23/src/test/scala/MySuite.scala
+++ day23/src/test/scala/MySuite.scala
@@ -3,10 +3,10 @@
 class MySuite extends munit.FunSuite:
   test("Day23 : main test on final data"):
     val (score1, score2) = Solver.solve
-    assertEquals(score1, "")
-    assertEquals(score2, "")
+    assertEquals(score1, "1327")
+    assertEquals(score2, "df,kg,la,mp,pb,qh,sk,th,vn,ww,xp,yp,zk")
 
   test("Day23 : main test on initial data"):
     val (score1, score2) = Solver.solveTest
-    assertEquals(score1, "")
-    assertEquals(score2, "")
+    assertEquals(score1, "7")
+    assertEquals(score2, "co,de,ka,ta")
