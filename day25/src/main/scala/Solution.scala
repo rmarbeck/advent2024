df6c9f5376316d9dccdbf12e9971f49386e1da65
rmarbeck <rmarbeck@free.fr>
2024-12-25 19:13:38 +0100

    Day 25 OK

--- day25/src/main/scala/Solution.scala
+++ day25/src/main/scala/Solution.scala
@@ -1,17 +1,32 @@
 object Solution:
   def run(inputLines: Seq[String]): (String, String) =
 
+    val keysAndLocks = inputLines.grouped(8).map(_.take(7)).toList.collect:
+      lines =>
+        val (head, meaningfull) = (lines.head, convert(lines.tail))
+        head.contains("#") match
+          case false => Key(meaningfull)
+          case true => Lock(meaningfull)
 
-    //
-    // Code is here
-    //
+    val keys = keysAndLocks.collect:
+      case k: Key => k
 
+    val locks = keysAndLocks.collect:
+      case l: Lock => l
 
+    val result1 = locks.map:
+      case l => keys.count(_.fits(l))
+    .sum
 
-    val result1 = s""
     val result2 = s""
 
     (s"$result1", s"$result2")
 
-end Solution
+case class Key(cols: Seq[Int]):
+  def fits(lock: Lock): Boolean = cols.zip(lock.cols).forall(_ + _ <= 6)
 
+case class Lock(cols: Seq[Int])
+
+def convert(lines: Seq[String]): Seq[Int] =
+  val transposed = lines.map(_.toCharArray).toArray.transpose
+  transposed.map(_.count(_ == '#')).toSeq
--- day25/src/test/scala/MySuite.scala
+++ day25/src/test/scala/MySuite.scala
@@ -3,10 +3,10 @@
 class MySuite extends munit.FunSuite:
   test("Day25 : main test on final data"):
     val (score1, score2) = Solver.solve
-    assertEquals(score1, "")
+    assertEquals(score1, "3690")
     assertEquals(score2, "")
 
   test("Day25 : main test on initial data"):
     val (score1, score2) = Solver.solveTest
-    assertEquals(score1, "")
+    assertEquals(score1, "3")
     assertEquals(score2, "")
