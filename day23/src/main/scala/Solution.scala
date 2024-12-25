89f3fdca776a928e9f2b45ca565ef016a6559055
rmarbeck <rmarbeck@free.fr>
2024-12-25 20:16:16 +0100

    Day 23 updated

--- day23/src/main/scala/Solution.scala
+++ day23/src/main/scala/Solution.scala
@@ -6,17 +6,19 @@ type Links = Map[String, Set[String]]
 object Solution:
   def run(inputLines: Seq[String]): (String, String) =
 
-    val grouped: Links = inputLines.flatMap:
+    val links: Links = inputLines.flatMap:
       case s"$first-$second" => List(first -> second, second -> first)
     .groupMapReduce(_._1)((_, other) => Set(other))(_ ++ _)
 
-    given Links = grouped
+    given Links = links
 
-    val result1 = grouped.filter(_._1.startsWith("t")).flatMap((key, values) => connected(values.toList, Nil).map((f, s) => alpha(key, f, s))).toList.distinct
+    val result1 = links.filter(_._1.startsWith("t")).flatMap:
+      (key, values) => connected(values, Set()).map((f, s) => alpha(key, f, s))
+    .toList.distinct
 
     import Order.given
 
-    val result2 = findBest(TreeSet(grouped.map((key, values) => values + key).map(_.toList).toList:_ *))
+    val result2 = findBest(TreeSet(links.map((key, values) => values + key).map(_.toList).toList:_ *))
 
     (s"${result1.size}", s"$result2")
 
@@ -28,23 +30,26 @@ def alpha(one: String, two: String, three: String): String =
 
 @tailrec
 def findBest(toExplore: TreeSet[List[String]])(using links: Links): String =
+  def isTheOne(toTest: Set[String]): Boolean =
+    toTest.forall:
+      member =>
+        val next = links(member) + member
+        (toTest intersect next) == toTest
   toExplore match
     case empty if empty.isEmpty => ""
     case notEmpty =>
-      val (head, tail) = (notEmpty.head, notEmpty.tail)
-      val matches = head.forall:
-        member =>
-          val next = links(member) + member
-          head.forall(next.contains)
-      matches match
-        case true => head.sorted.mkString(",")
-        case false => findBest(tail ++ head.combinations(head.size - 1))
+      val head = notEmpty.head
+      if isTheOne(head.toSet) then
+        head.sorted.mkString(",")
+      else
+        findBest(notEmpty.tail ++ head.combinations(head.size - 1))
 
 @tailrec
-def connected(list: List[String], current: List[(String, String)])(using links: Links): List[(String, String)] =
-  list match
-    case Nil => current
-    case head :: tail =>
+def connected(input: Set[String], current: Set[(String, String)])(using links: Links): Set[(String, String)] =
+  input match
+    case empty if empty.isEmpty => current
+    case notEmpty =>
+      val (head, tail) = (notEmpty.head, notEmpty.tail)
       val updatedList = tail.collect:
         case currentComputerInTail if links(currentComputerInTail).contains(head) => (currentComputerInTail, head)
-      connected(tail, updatedList ::: current)
+      connected(tail, updatedList ++ current)
