3e280ac3bc884f3f00c4cc7d751f2b54624ee454
rmarbeck <rafoufou>
2024-12-31 12:44:25 +0100

    Day23 (using a dedicated case class for TreeSet parameter 2,2s => 0,1s)

--- day23/src/main/scala/Solution.scala
+++ day23/src/main/scala/Solution.scala
@@ -16,20 +16,25 @@ object Solution:
       (key, values) => connected(values - key).map(alpha(key, _, _))
     .toSet
 
-    import Order.given
-
-    val result2 = findBest(TreeSet(links.map((key, values) => values + key).map(_.toList).toSeq:_ *))
+    val result2 = findBest(TreeSet(links.map((key, values) => values + key).map(l => Computers.apply(l.toList)).toSeq:_ *))
 
     (s"${result1.size}", s"$result2")
-
-object Order:
-  given orderingListOfString: Ordering[List[String]] = Ordering.by(str => (-str.length, str.sorted.mkString))
 
 def alpha(threes: String*): String =
   List(threes:_ *).sorted.mkString("-")
 
+case class Computers private (names: Set[String], order: Int):
+  override lazy val toString: String = names.toList.sorted.mkString(",")
+  private lazy val length: Int = names.size
+  lazy val combos: Iterator[Computers] = names.toList.combinations(length - 1).map(Computers.apply)
+  def addComputer(name: String): Computers = Computers(names + name, order + name.hashCode)
+
+object Computers:
+  def apply(names: List[String]): Computers = Computers(names.toSet, names.hashCode())
+  given orderingListOfString: Ordering[Computers] = Ordering.by(computers => (-computers.length, computers.order))
+
 @tailrec
-def findBest(toExplore: TreeSet[List[String]])(using links: Links): String =
+def findBest(toExplore: TreeSet[Computers])(using links: Links): String =
   def isTheOne(toTest: Set[String]): Boolean =
     toTest.forall:
       member => toTest.subsetOf(links(member))
@@ -37,10 +42,10 @@ def findBest(toExplore: TreeSet[List[String]])(using links: Links): String =
     case empty if empty.isEmpty => ""
     case notEmpty =>
       val head = notEmpty.head
-      if isTheOne(head.toSet) then
-        head.sorted.mkString(",")
+      if isTheOne(head.names) then
+        head.toString
       else
-        findBest(notEmpty.tail ++ head.combinations(head.size - 1))
+        findBest(notEmpty.tail ++ head.combos)
 
 @tailrec
 def connected(input: Set[String], current: Set[(String, String)] = Set())(using links: Links): Set[(String, String)] =
